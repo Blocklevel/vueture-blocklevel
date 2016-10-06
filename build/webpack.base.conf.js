f949430626e0c793f30667fe378c2623efe6b9c0
Tim Rijkse | Blocklevel <tim@blocklevel.nl>
2016-10-06 18:19:10 +0200

    Add font alias

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -18,6 +18,7 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
+      'fonts': path.resolve(__dirname, '../src/assets/fonts'),
       'components': path.resolve(__dirname, '../src/app/components'),
       'vue': 'vue/dist/vue.js'
     }
